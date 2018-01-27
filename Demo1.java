d03730af75d35564374593d66fb14846089b3602
username <user@mail.com>
2018-01-27 10:35:16 +0800

    utf8

--- Demo1.java
+++ Demo1.java
@@ -1,93 +1,31 @@
-package day15;
-import java.util.*;
+package day02;
+
 public class Demo1 {
    public static void main(String[] args) {
-//	  Scanner sc = new Scanner(System.in);
-//	  int num=sc.nextInt();
-	  Car car1=CarFactory.getInstance(1);
-	  Car car2=CarFactory.getInstance(2);
-	  Car car3=CarFactory.getInstance(3);
-	  car1.start();
-	  car2.start();
-	  car3.start();
-//	  car.run();
-//	  car.stop();
+	int a=10000;
+	byte b=2;
+	short c=100;
+	long e=1000000000;
+	float f=1.2f;
+	double g=1.23234123;
+	char h= '中';
+	char i='a';
+	boolean j=true;
+	boolean k=false;
+	boolean l=100>=90;
+	String m="今天盗汗了！";
+	String n="热！！";
+	System.out.println(m+n);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(e);
+	System.out.println(f);
+	System.out.println(g);
+	System.out.println(h);
+	System.out.println(i);
+	System.out.println(j);
+	System.out.println(k);
+	System.out.println(l);
 }
 }
- class CarFactory{
- public static  Car getInstance(int type) {
-		Car car=null;
-		if(type==1) {
-			car=new Bmw();
-		}else if(type==2) {
-			car=new Taxi();
-		}else if(type==3) {
-			car =new QQ();
-		}
-		return car;
-	}
-}
-interface Car{
-	void start();
-	void run();
-	void stop();
-}
-class Taxi implements Car{
-	@Override
-	public void start() {
-		 System.out.println("出租启动");
-		
-	}
-
-	@Override
-	public void run() {
-		System.out.println("出租行驶");
-		
-	}
-
-	@Override
-	public void stop() {
-		System.out.println("出租刹车");
-		
-}
-}
-class Bmw implements Car{
-	@Override
-	public void start() {
-		 System.out.println("奔驰启动");
-		
-	}
-
-	@Override
-	public void run() {
-		System.out.println("奔驰行驶");
-		
-	}
-
-	@Override
-	public void stop() {
-		System.out.println("奔驰刹车");
-		
-}
-}
- class QQ implements Car{
-
-	@Override
-	public void start() {
-		 System.out.println("QQ启动");
-		
-	}
-
-	@Override
-	public void run() {
-		System.out.println("QQ行驶");
-		
-	}
-
-	@Override
-	public void stop() {
-		System.out.println("QQ刹车");
-		
-	}
-}
-
